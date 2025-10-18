d9f5f5fb3326003025ec5b5b0ae0d216524cdc8a
ritam413 <ritamm413@gmail.com>
2025-10-18 21:41:27 +0530

    Origin allowing

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
+app.get('/test', (req, res) => res.json('Hello World!'))
 
 
 //import routes
