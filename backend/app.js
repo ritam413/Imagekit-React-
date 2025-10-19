58678424eab8a4b5daf2eb0bba046b473ffe0bd1
ritam413 <ritamm413@gmail.com>
2025-10-19 10:36:45 +0530

    developemnt Changes , CORS fix

--- backend/app.js
+++ backend/app.js
@@ -7,19 +7,12 @@ const app = express()
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const allowedOrigins = [import.meta.env.FRONTEND_URL]
+const allowedOrigins = import.meta.env.FRONTEND_URL
 
 app.use(cors({
-
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
-}));
+}))
 
 app.use(cookieParser())
 app.use(express.json())
