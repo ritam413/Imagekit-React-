26d8a6f302b14e4caa49bac715aca98c14f43aad
ritam413 <ritamm413@gmail.com>
2025-10-19 13:17:33 +0530

    developemnt Changes , cookie issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 //---- React Imports ----
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
-import { Routes,Route,Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
-import {api} from "../src/utils/axiosInstance.js"
+import { api } from "../src/utils/axiosInstance.js"
 //---- Pages Imports ----
 import LoginPage from './Pages/LoginPage.jsx'
 import SignupPage from './Pages/SignUpPage.jsx'
@@ -15,50 +15,55 @@ import UserDashboard from './Pages/DashBoard.jsx'
 import { EditPage } from './Pages/EditPage.jsx'
 // ---------------------
 function App() {
-  
+
   // ---- States ----
   const [loading, setLoading] = useState(true)
 
   // ---- Check if backend is online ----
-  const isBackendOnline = async()=>{
+  const isBackendOnline = async () => {
     try {
-      
+
       const res = await api.get("/isOnline")
-  
-      const data =  res.data
-  
-      if(data){
+
+      const data = res.data
+
+      if (data) {
         setLoading(false)
         toast.success(data.message)
       }
     } catch (error) {
       setLoading(true)
-    }finally{
+    } finally {
       setLoading(false)
     }
   }
-  
-  console.log(api.defaults.baseURL)
-  
-  const interval = setInterval(() => {
-    useEffect(()=>{
-      const status =isBackendOnline()
-      if(status===200){
-        setLoading(false);
-        clearInterval(interval);
-      }
 
-  },[])  
-  }, 5000);
-  
+  console.log(api.defaults.baseURL)
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const status = await isBackendOnline(); // assuming this returns a Promise
+        if (status === 200) {
+          setLoading(false);
+          clearInterval(interval);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }, 5000);
+
+    return () => clearInterval(interval); // cleanup on unmount
+  }, []); // runs once on moun
+
   // ------------------------------------
-  
+
   return (
     <>
-    {/* Declaring Toaster */}
-    <Toaster position='top-center'/>
-    
-    {/* ----- Routes ----- */}
+      {/* Declaring Toaster */}
+      <Toaster position='top-center' />
+
+      {/* ----- Routes ----- */}
       <Routes>
         <Route
           path='/'
@@ -93,7 +98,7 @@ function App() {
           element={<EditPage />}
         ></Route>
       </Routes>
-    {/* ------------------ */}
+      {/* ------------------ */}
     </>
   )
 }
