56a8a30d3b5f8c48f4b1a00c397168b3fd1548db
ritam413 <ritamm413@gmail.com>
2025-10-19 09:57:30 +0530

    developemnt Changes , CORS fix

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.BACKEND_URL || 
+const baseURL = import.meta.env.VITE_BACKEND_URL || 
   (import.meta.env.PROD?
     import.meta.env.VITE_BACKEND_URL
     :
