9676d312bd704ebc92b8c06fbbd7a8ffa940d14f
ritam413 <ritamm413@gmail.com>
2026-02-01 21:02:35 +0530

    fix cors issue

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,18 @@ const app = express()
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const netlifyRegex = /^https:\/\/.*--picxy\.netlify\.app$/;
 const allowedOrigins = ["https://picxy.netlify.app", "http://localhost:5173"];
 
 app.use(cors({
+
   origin: function(origin, callback) {
     if (!origin) return callback(null, true); // Postman / server-to-server
-    if (allowedOrigins.includes(origin) || netlifyRegex.test(origin)) return callback(null, true);
-    return callback(new Error(`CORS not allowed for origin: ${origin}`));
+    
+    if(allowedOrigins.includes(origin)){
+      return callback(null,true)
+    }
+
+    return callback(new Error('Not allowed by CORS'));
   },
   credentials: true
 }));
