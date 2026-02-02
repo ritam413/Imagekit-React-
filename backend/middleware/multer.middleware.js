52e590ae439aecced1fb903da745ee6208985fe7
ritam413 <ritamm413@gmail.com>
2026-02-02 23:49:22 +0530

    fix uploadmedia Controller

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -1,6 +1,9 @@
 import multer from "multer"
 import path from "path"
 import { fileURLToPath } from "url"
+import os from 'os'
+import fs from "fs";
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -8,9 +11,12 @@ const __dirname = path.dirname(__filename)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         if(process.env.NODE_ENV === "production")
-            cb(null, path.join(__dirname, "/tmp/"))
-        else
+        {
+            cb(null,os.tmpdir())
+        }else{
             cb(null, path.join(__dirname, "../public/temp"))
+
+        }
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
